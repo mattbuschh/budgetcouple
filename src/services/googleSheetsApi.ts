73fd9556444a85cf2998c0fbd2cb96f1c6bd265c
mattbuschh <168812572+mattbuschh@users.noreply.github.com>
2025-07-16 10:55:34 +0200

    Updated googleSheetsApi.ts

--- src/services/googleSheetsApi.ts
+++ src/services/googleSheetsApi.ts
@@ -10,7 +10,7 @@ export interface LigneBudget {
   mois: string;
 }
 
-const API_URL = 'https://v1.nocodeapi.com/mattbusch/google_sheets/leEhXUyGQcrZAMIJ?tabId=Feuille1';
+const API_URL = 'https://v1.nocodeapi.com/mattbusch/google_sheets/leEhXUyGQcrZAMIJ?tabId=Feuille 1';
 
 /**
  * Ajoute une ligne au Google Sheet
